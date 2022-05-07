04c1f319c29e73e2ac1ab82297635c4743a24555
Yoga14s\CXIN <genius_cx@qq.com>
2022-05-07 22:34:30 +0800

    内嵌MsgPackRPC

---
+++ src/rpc.jl
@@ -0,0 +1,44 @@
+const REQUEST = 0
+
+mutable struct RPCSession{S}
+    sock::S
+    msgid::Int
+end
+
+RPCSession(port::Integer) = RPCSession(Sockets.connect(port), 1)
+RPCSession(addr::IPAddr, port::Integer) = RPCSession(Sockets.connect(addr, port), 1)
+
+Base.close(s::RPCSession) = close(s.sock)
+
+function call(session, method, params...; timeout=0.001, interval=0.001)
+    msgid = session.msgid
+    session.msgid += 1
+    msg = pack([REQUEST, msgid, method, params])
+    write(session.sock, msg)
+    # ! assume work perfectly ...
+    res = unpack(readavailable(session.sock))
+    # [1, msgid, error, result]
+    @inbounds if res[1] != 1 || res[2] != msgid || !isnothing(res[3])
+        # TODO throw error?
+        nothing
+    else
+        res[4]
+    end
+end
+
+function recvbytes(sock; timeout=0.001, interval=0.001)
+    res = nothing
+    ch = Channel{Bool}(1)
+    t = @async begin
+        isready(ch) && return
+        res = readavailable(sock)
+    end
+    times = Int(timeout / interval)
+    while times > 0
+        sleep(interval)
+        istaskdone(t) && return res
+        times -= 1
+    end
+    put!(ch, true)
+    return res
+end
